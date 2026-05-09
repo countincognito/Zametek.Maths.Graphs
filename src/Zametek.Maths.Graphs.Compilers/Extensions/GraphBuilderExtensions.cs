33b7942d0c9f22fd4c253eaf56a17cc3258fbcc9
Will Anderson <will.anderson@neurontechnologies.ai>
2026-05-09 17:26:31 -0500

    fix: algorithm bugs — Tarjan O(n) stack check, KeyExtensions reflection hotpath; cleanup unused usings, dead code, misspellings

--- src/Zametek.Maths.Graphs.Compilers/Extensions/GraphBuilderExtensions.cs
+++ src/Zametek.Maths.Graphs.Compilers/Extensions/GraphBuilderExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
 namespace Zametek.Maths.Graphs
 {
