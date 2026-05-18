4e8303cf2148182a16b0afeb59b669a52dd63e0d
countincognito <countincognito@outlook.com>
2026-05-18 17:49:27 +0100

    Add brackets to if statements.

--- src/Zametek.Maths.Graphs.Compilers/Extensions/KeyExtensions.cs
+++ src/Zametek.Maths.Graphs.Compilers/Extensions/KeyExtensions.cs
@@ -8,9 +8,13 @@ namespace Zametek.Maths.Graphs
             where T : struct, IComparable<T>, IEquatable<T>
         {
             if (typeof(T) == typeof(int))
+            {
                 return (T)(object)((int)(object)input + 1);
+            }
             if (typeof(T) == typeof(Guid))
+            {
                 return (T)(object)Guid.NewGuid();
+            }
             throw new InvalidOperationException($"Type {typeof(T)} is not supported for key generation.");
         }
 
@@ -18,9 +22,13 @@ namespace Zametek.Maths.Graphs
             where T : struct, IComparable<T>, IEquatable<T>
         {
             if (typeof(T) == typeof(int))
+            {
                 return (T)(object)((int)(object)input - 1);
+            }
             if (typeof(T) == typeof(Guid))
+            {
                 throw new InvalidOperationException("Guid keys do not support Previous.");
+            }
             throw new InvalidOperationException($"Type {typeof(T)} is not supported for key generation.");
         }
     }
